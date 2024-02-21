b796b25e15365e596c2cc4b2fe58aaac26ee4822
Pavel Buryka <l1kaf@yandex.ru>
2024-02-21 22:24:43 +0300

    add resource routing

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -13,6 +13,6 @@
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
         <a href="{{ route('articles.edit', $article->id) }}">Редактировать</a>
-        <a href="/articles/{{$article->id}}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
+        <a href="{{ route('articles.destroy', $article->id) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
     @endforeach
 @endsection
